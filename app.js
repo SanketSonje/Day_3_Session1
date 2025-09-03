528017c21f87f16b58dcba9a5b95e6856d68b26f
Sanket Pramod Sonje <119783619+SanketSonje@users.noreply.github.com>
2025-09-03 14:56:47 +0530

    JS debug Config

---
+++ app.js
@@ -0,0 +1,21 @@
+// app.js
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+let num1 = 5;
+let num2 = 10;
+
+console.log("Starting program...");
+
+let sum = add(num1, num2);
+console.log("Sum:", sum);
+
+let product = multiply(num1, num2);
+console.log("Product:", product);
+
+console.log("Program finished.");
